85ab79c3dccf71f71b4df25be73b63131112f032
Ebube <dbupanyiam@gmail.com>
2022-01-28 23:48:10 +0100

    Welcome Function created

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ import inquirer from "inquirer";
 import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+let player_name;
+
+const timeout = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcomeText() {
+  const rainbow_title = chalkAnimation.rainbow(
+    "Are you Apex's best quizzer \n"
+  );
+
+  await timeout();
+  rainbow_title.stop();
+
+  console.log(`
+  ${chalk.bgBlue("HOW TO PLAY")}
+  I am a process on your computer.
+  If you get any question I will be ${chalk.bgRed("killed")}
+  So get all the questions right...
+  `);
+}
